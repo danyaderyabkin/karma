70d2d00e92db21ccf7c999359b467da2e35c33d6
Deryabkin Daniil <danya.deryabkin98@gmail.com>
2023-12-27 17:25:10 +0300

    deploy v1.5

--- main.js
+++ main.js
@@ -3,10 +3,6 @@
 import 'animate.css';
 import './public/assets/css/style.css';
 
-import './public/assets/js/jquery-3.7.1.min.js';
-import './public/assets/bootstrap/js/bootstrap.min.js';
-import './public/assets/js/validation.js';
-import './public/assets/js/swiper.min.js';
 //Анимация
 new WOW().init();
 
