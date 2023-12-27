f1208bfa79727be439835e437adbf86280c083f2
Deryabkin Daniil <danya.deryabkin98@gmail.com>
2023-12-27 16:01:10 +0300

    commit

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-import './style.css'
-(() => {
-    const swiper = new Swiper('.js-hero-slider', {
-        slidesPerView: 1,
-        loop: false,
-        pagination: {
-            el: '.js-pagination',
-            clickable: true,
-        },
-        a11y: {
-            paginationBulletMessage: 'К следущему {{index}}',
-        },
-    });
-})();
+//Стили
+import 'animate.css';
+import './public/assets/css/style.css';
+
+//Анимация
+new WOW().init();
+
+//Слайдер
+const swiper = new Swiper('.js-hero-slider', {
+    slidesPerView: 1,
+    loop: false,
+    pagination: {
+        el: '.js-pagination',
+        clickable: true,
+    },
+    a11y: {
+        paginationBulletMessage: 'К следущему {{index}}',
+    },
+});
 
 
--- public/assets/js/accordion.js
+++
@@ -1,3 +0,0 @@
-(() => {
-  new Accordion(".accordion-container");
-})();
